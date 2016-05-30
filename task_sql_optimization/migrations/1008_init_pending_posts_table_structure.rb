29e1e4b64acd0729a4f7a867300e1ea5b637940e
Alex Kalinin <login.hedin@gmail.com>
2016-05-30 23:27:25 +0600

    #4 prepared the structure of pending_posts table and it's references tables

---
+++ task_sql_optimization/migrations/1008_init_pending_posts_table_structure.rb
@@ -0,0 +1,77 @@
+migration_id = '[Migration 1008_init_pending_posts_table_structure.rb]'
+
+$logger.info "#{migration_id}: Strarting migration"
+
+sql = <<END_SQL.gsub(/\s+/, ' ').strip
+
+CREATE TABLE posts
+(
+  id serial NOT NULL,
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone,
+  CONSTRAINT posts_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+CREATE TABLE users
+(
+  id serial NOT NULL,
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone,
+  CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE pending_posts
+(
+  id serial NOT NULL,
+  post_id integer NOT NULL,
+  user_id integer NOT NULL,
+  approved boolean NOT NULL,
+  banned boolean NOT NULL,
+  CONSTRAINT pending_posts_pkey PRIMARY KEY (id),
+  CONSTRAINT pending_posts_post_id_fkey FOREIGN KEY (post_id)
+      REFERENCES posts (id) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT pending_posts_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+
+
+CREATE TABLE viewed_posts
+(
+  id serial NOT NULL,
+  user_id integer NOT NULL,
+  post_id integer NOT NULL,
+  CONSTRAINT viewed_posts_pkey PRIMARY KEY (id),
+  CONSTRAINT viewed_posts_post_id_fkey FOREIGN KEY (post_id)
+      REFERENCES posts (id) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT,
+  CONSTRAINT viewed_posts_user_id_fkey FOREIGN KEY (user_id)
+      REFERENCES users (id) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE RESTRICT
+)
+WITH (
+  OIDS=FALSE
+);
+
+END_SQL
+
+
+db = DbHelper.new $APP_CONFIG['pg_host'], $APP_CONFIG['pg_port'], $APP_CONFIG['pg_login'],
+                  $APP_CONFIG['pg_password'], $APP_CONFIG['pg_database']
+db.exec_query(sql)
+db.disconnect
